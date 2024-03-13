f1b8e36ae3b75b799e3690cb0f78c035e41e901b
kishan2624 <ftd10822422@dseu.ac.in>
2024-03-13 22:50:42 +0530

    update one by one turn

--- Event/board.js
+++ Event/board.js
@@ -1,46 +1,47 @@
-import * as help from "../Helper/constant.js"
+import * as help from "../Helper/constant.js";
 
-let isWhiteTurn = true
+let isWhiteTurn = true;
 
-export function setupBoardSquares(){
-    for(let i=0 ; i<help.boardSquares.length;i++){
-        help.boardSquares[i].addEventListener("dragover",allowDrop);
-        help.boardSquares[i].addEventListener("drop",drop);
-        
-    }
+export function setupBoardSquares() {
+  for (let i = 0; i < help.boardSquares.length; i++) {
+    help.boardSquares[i].addEventListener("dragover", allowDrop);
+    help.boardSquares[i].addEventListener("drop", drop);
+  }
 }
 
-export function setupPieces(){
-    for(let i=0;i<help.pieces.length;i++){
-        help.pieces[i].addEventListener("dragstart",drag);
-        help.pieces[i].setAttribute("draggable",true);
-        help.pieces[i].id = help.pieces[i].className.split(" ")+help.pieces[i].parentElement.id
-
-    }
-    for(let i=0;i<help.piecesImages.length;i++){
-        help.piecesImages[i].setAttribute("draggable",false);
-    }
+export function setupPieces() {
+  for (let i = 0; i < help.pieces.length; i++) {
+    help.pieces[i].addEventListener("dragstart", drag);
+    help.pieces[i].setAttribute("draggable", true);
+    help.pieces[i].id =
+      help.pieces[i].className.split(" ") + help.pieces[i].parentElement.id;
+  }
+  for (let i = 0; i < help.piecesImages.length; i++) {
+    help.piecesImages[i].setAttribute("draggable", false);
+  }
 }
 
-function allowDrop(ev){
-    ev.preventDefault();
+function allowDrop(ev) {
+  ev.preventDefault();
 }
 
-function drag(ev){
-    const piece = ev.target;
-    const pieceColor = piece.getAttribute("color")
-    if((isWhiteTurn && pieceColor == "white") || (!isWhiteTurn && pieceColor == "black")){
-        ev.dataTransfer.setData("text",piece.id);
-    }
-    
+function drag(ev) {
+  const piece = ev.target;
+  const pieceColor = piece.getAttribute("color");
+  if (
+    (isWhiteTurn && pieceColor == "white") ||
+    (!isWhiteTurn && pieceColor == "black")
+  ) {
+    ev.dataTransfer.setData("text", piece.id);
+  }
 }
 
-function drop(ev){
-    ev.preventDefault()
-    let data = ev.dataTransfer.getData("text")
-    const piece = document.getElementById(data);
-    const destinationSquare= ev.currentTarget;
-    const destinationSquareId = destinationSquare.id;
-    destinationSquare.appendChild(piece);
-    isWhiteTurn = !isWhiteTurn;
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  const piece = document.getElementById(data);
+  const destinationSquare = ev.currentTarget;
+  const destinationSquareId = destinationSquare.id;
+  destinationSquare.appendChild(piece);
+  isWhiteTurn = !isWhiteTurn;
 }
